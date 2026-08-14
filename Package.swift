00a6ba6b4cb86aa111d4f5d8715c6730bf66f6d6
fhasse95 <49185957+fhasse95@users.noreply.github.com>
2026-08-14 16:28:12 +0200

    Add platform compatibility updates

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
     name: "skip-revenue",
     defaultLocalization: "en",
-    platforms: [.iOS(.v17), .macOS(.v14)],
+    platforms: [.iOS(.v16), .macOS(.v13), .macCatalyst(.v16)],
     products: [
-        .library(name: "SkipRevenueUI", type: .dynamic, targets: ["SkipRevenueUI"]),
-        .library(name: "SkipRevenue", type: .dynamic, targets: ["SkipRevenue"]),
+        .library(name: "SkipRevenueUI", targets: ["SkipRevenueUI"]),
+        .library(name: "SkipRevenue", targets: ["SkipRevenue"]),
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "1.6.36"),
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .target(name: "SkipRevenue", dependencies: [
             .product(name: "SkipFoundation", package: "skip-foundation"),
-            .product(name: "RevenueCat", package: "purchases-ios", condition: .when(platforms: [.iOS, .macOS]))
+            .product(name: "RevenueCat", package: "purchases-ios", condition: .when(platforms: [.iOS, .macOS, .macCatalyst]))
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "SkipRevenueTests", dependencies: [
             "SkipRevenue",
@@ -27,7 +27,7 @@ let package = Package(
         .target(name: "SkipRevenueUI", dependencies: [
             "SkipRevenue",
             .product(name: "SkipUI", package: "skip-ui"),
-            .product(name: "RevenueCatUI", package: "purchases-ios", condition: .when(platforms: [.iOS, .macOS]))
+            .product(name: "RevenueCatUI", package: "purchases-ios", condition: .when(platforms: [.iOS, .macOS, .macCatalyst]))
         ], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "SkipRevenueUITests", dependencies: [
             "SkipRevenueUI",
